e5c0b2014e3a06fd06c89eaf65348837da64cf03
@_taeung__ <155827534+ehdnd@users.noreply.github.com>
2024-03-18 12:39:23 +0900

    Updated week3 2c

--- week3/main.c
+++ week3/main.c
@@ -102,3 +102,109 @@ int main(void)
 
 */
 
+/* #3-5 p.57
+
+#include <stdio.h>
+
+int main(void)
+{
+	char ch1 = 'A';
+	char ch2 = 65;
+
+	printf("문자 %c의 아스키 코드 값: %d\n", ch1, ch1);
+	printf("아스키 코드 값이 %d인 문자: %c\n", ch2, ch2);
+
+	return 0;
+}
+
+*/
+
+/* #3-6 p.58
+
+#include <stdio.h>
+
+int main(void) {
+	short sh = 32767;
+	int in = 2147483647;
+	long ln = 214783647;
+	long long lln = 123451234512345;
+
+	printf("short형 변수 출력: %d\n", sh);
+	printf("int형 변수 출력: %d\n", in);
+	printf("long형 변수 출력: %ld\n", ln);
+	printf("long long형 변수 출력: %lld\n", lln);
+
+	return 0;
+}
+
+*/
+
+/* #3-7 p.60
+
+#include <stdio.h>
+
+int main(void)
+{
+	unsigned int a;
+
+	a = 4294967295;
+	printf("%d\n", a);
+
+	a = -1;
+	printf("%u\n", a);
+
+	return 0;
+}
+
+*/
+
+/* #2-8 p.62
+
+#include <stdio.h>
+
+int main(void)
+{
+	float ft = 1.234567890123456789;
+	double db = 1.234567890123456789;
+
+	printf("float형 변수의 값: %.20f\n", ft);
+	printf("double형 변수의 값: %.20lf\n", db);
+
+	return 0;
+}
+
+*/
+
+/* #2-9 p.64
+
+#include <stdio.h>
+
+int main(void)
+{
+	char fruit[28] = "strawberry";
+
+	printf("딸기: %s\n", fruit);
+	printf("딸기쨈: %s %s\n", fruit, "jam");
+
+	return 0;
+}
+
+*/
+
+/* #2-10 p.65
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	char fruit[28] = "strawberry";
+
+	printf("%s\n", fruit);
+	strcpy(fruit, "banana");
+	printf("%s\n", fruit);
+
+	return 0;
+}
+
+*/
