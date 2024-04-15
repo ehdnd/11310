78f7ade2dbeafb4e8930c35a2fc503f88f3d766a
@_taeung__ <155827534+ehdnd@users.noreply.github.com>
2024-04-15 11:45:27 +0900

    Updated week7 etc

--- etc/etc4.c
+++ etc/etc4.c
@@ -1,26 +1,23 @@
-//// for문에서 전위연산자와 후위연산자에 차이가있는가?
-//#include <stdio.h>
-//
-//int main(void)
-//{
-//	int i, j;
-//
-//	printf("i++ 시작\n"); //i++ 시작 표시 출력
-//	for (i = 0; i < 5; i++)
-//	{
-//		printf("후위연산자 i = %d\n", i); //'for문 내'에서 i변화 추적
-//	}
-//
-//	printf("\n");
-//
-//	printf("++j 시작\n"); // ++j 시작 표시 출력
-//	for (j = 0; j < 5; ++j)
-//	{
-//		printf("전위연산자 j = %d\n", j); //'for문 내'에서 j변화 추적
-//	}
-//
-//	return 0;
-//}
-//// 실행결과는 아래와 같다.
-//// 0회1회2회3회4회i++ 종료
-//// 0회1회2회3회4회++j 종료
+// for문에서 전위연산자와 후위연산자에 차이가있는가?
+#include <stdio.h>
+
+int main(void)
+{
+	int i, j;
+
+	printf("i++ 시작\n"); //i++ 시작 표시 출력
+	for (i = 0; i < 5; i++)
+	{
+		printf("후위연산자 i = %d\n", i); //'for문 내'에서 i변화 추적
+	}
+
+	printf("\n");
+
+	printf("++j 시작\n"); // ++j 시작 표시 출력
+	for (j = 0; j < 5; ++j)
+	{
+		printf("전위연산자 j = %d\n", j); //'for문 내'에서 j변화 추적
+	}
+
+	return 0;
+}
